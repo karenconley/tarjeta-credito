512e9038e20771c7ee24aafc94f0f6ef60d9f1ea
karenconley <karen.c.echeverria@gmail.com>
2017-10-05 10:09:35 -0300

    tarjeta credito actualizacion

---
+++ app.js
@@ -0,0 +1,13 @@
+//preguntar por número de tarjeta:
+var creditCard = prompt("Ingrese su número de tarjeta");
+
+function number (){
+  var result = [creditCard];
+  //se recorre el número ingresado
+  for (var i = 0; i < creditCard.length; i++);
+  result.push(creditCard[i]);
+  if(numberCard !== 'number' || numberCard === 0){
+}
+var newResult = result.reverse();
+console.log(newResult);
+}
